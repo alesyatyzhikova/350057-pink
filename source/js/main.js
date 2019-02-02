e4e3985764d80268274ef45a2266461073414aa5
Алеся Тужикова <alesya_tyzhikova@Alesas-MacBook-Air.local>
2019-02-02 20:04:24 +0300

    Окончательный вариант

--- source/js/main.js
+++ source/js/main.js
@@ -1,14 +1,22 @@
-var navMain = document.querySelector('.main-navigation');
-var navToggle = document.querySelector('.main-navigation__toggle');
+var navMain = document.querySelector('.nav__list');
+var navOpenButton = document.querySelector('.nav__button');
+var navLogo = document.querySelector('.logo__wrapper');
 
 navMain.classList.remove("main-navigation--nojs");
 
-navToggle.addEventListener('click', function() {
-  if (navMain.classList.contains('main-navigation--closed')) {
-    navMain.classList.remove('main-navigation--closed');
-    navMain.classList.add('main-navigation--opened');
+navOpenButton.addEventListener('click', function() {
+  if (navMain.classList.contains('nav--closed')) {
+    navMain.classList.remove('nav--closed');
+    navMain.classList.add('nav--opened');
+    navLogo.classList.add('logo__wrapper--open-menu');
+    navOpenButton.classList.remove('nav__button');
+    navOpenButton.classList.add('nav__button--closed');
   } else {
-    navMain.classList.remove('main-navigation--opened');
-    navMain.classList.add('main-navigation--closed');
+    navMain.classList.remove('nav--opened');
+    navMain.classList.add('nav--closed');
+    navOpenButton.classList.remove('nav__button--closed');
+    navOpenButton.classList.add('nav__button');
+    navLogo.classList.remove('logo__wrapper--open-menu');
+    navLogo.classList.add('logo__wrapper');
   }
 });
